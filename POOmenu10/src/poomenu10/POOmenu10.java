8e9f45874f5deb06b6a0bbc80dd94cf27dda9c13
unknown <fabiomorales5608@gmail.com>
2023-07-06 00:06:45 -0600

    Subes el proyecto a github

--- POOmenu10/src/poomenu10/POOmenu10.java
+++ POOmenu10/src/poomenu10/POOmenu10.java
@@ -1,20 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package poomenu10;
 
-/**
- *
- * @author Morales
- */
+import java.util.Scanner;
+
 public class POOmenu10 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // 
+        Scanner entrada = new Scanner(System.in);
+        int op = 0;
+        try {
+            do {
+                System.out.println("    1. Crear Profesor");
+                System.out.println("    2. Ver Profesores");
+                System.out.println("    3. Ver Profesor");
+                System.out.println("    4. Actulizar Datos del Profesor");
+                System.out.println("    5. Salir");
+                System.out.println("");
+
+                op = entrada.nextInt();
+                switch (op) {
+                    case 1:
+                        System.out.println("-------------Nuevo Profesor-------------");
+                        break;
+
+                    case 2:
+                        System.out.println("------------Los_Datos_Son----------------");
+                        break;
+
+                    case 3:
+                        System.out.println("");
+                        break;
+
+                    case 4:
+                        System.out.println("");
+                        break;
+
+                    case 5:
+                        System.out.println("");
+                        System.out.println("_______Finalizado______");
+                        System.out.println("");
+                        break;
+
+                }
+            } while (op != 5);
+        } catch (Exception e) {
+            System.out.println("Palabra Incorrecta");
+        }
     }
-    
+
 }
---
+++ POOmenu10/src/poomenu10/Profesor.java
@@ -0,0 +1,34 @@
+
+package poomenu10;
+
+public class Profesor {
+    //Atributoa
+    private String Nombre,Materia;
+
+    public Profesor(String Nombre, String Materia) {
+        this.Nombre = Nombre;
+        this.Materia = Materia;
+    }
+    
+    
+    
+    
+    
+     //Getters and Setters
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getMateria() {
+        return Materia;
+    }
+
+    public void setMateria(String Materia) {
+        this.Materia = Materia;
+    }
+    
+}
